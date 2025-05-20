9e7153356910724be3146125e3b28dbb52ee2c83
ganeshhp <ganeshhp@gmail.com>
2025-05-20 23:43:00 +0530

    nfs-server-install script updated

--- Day-3/install-nfs-server.sh
+++ Day-3/install-nfs-server.sh
@@ -14,6 +14,8 @@ sudo chmod 777 /mnt/nfs_share/
 
 sudo echo -e "/exports/volumes *(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
 
+mkdir /exports/volumes
+
 sudo exportfs -a
 
 sudo systemctl restart nfs-kernel-server

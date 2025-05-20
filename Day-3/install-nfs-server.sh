3a097767e38afa8db377fe358f90a25b53ba84c2
ganeshhp <ganeshhp@gmail.com>
2025-05-20 23:38:34 +0530

    day-3 documents added

---
+++ Day-3/install-nfs-server.sh
@@ -0,0 +1,19 @@
+### install nfs-kernel server
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y nfs-kernel-server
+
+## create nfs share directory
+
+sudo mkdir -p /mnt/nfs_share
+
+sudo chown -R nobody:nogroup /mnt/nfs_share/
+
+sudo chmod 777 /mnt/nfs_share/
+
+sudo echo -e "/exports/volumes *(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
+
+sudo exportfs -a
+
+sudo systemctl restart nfs-kernel-server
